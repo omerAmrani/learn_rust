fd5ddca52d0709ce8b6c400dc357d694352a3f37
Omer Amrani <omer@airwayz.co>
2024-07-15 20:52:55 +0300

    Add primitives

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
+
+    // Primitives
+    let is_something: bool = true;
+    let number: i32 = 3433;
+    let mut block = "aa";
+    println!("number {number} bool {is_something} str {block}");
+
+    block = "AA";
+    println!("{block} has changed")
 }
